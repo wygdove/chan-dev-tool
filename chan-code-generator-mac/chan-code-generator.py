bc09ea22f4ea67b4fb5d236f5f4250d56e4468a7
weiyg <weiyg@asiainfo.com>
2019-06-12 16:37:05 +0800

    chan code generator mac fresh good

--- chan-code-generator-mac/chan-code-generator.py
+++ chan-code-generator-mac/chan-code-generator.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 __author__='wygdove'
-__time__='2018/4/26 10:56'
+__time__='2019/01/16 10:56'
 
 import os
 import time
@@ -9,22 +9,27 @@ import json
 # ------ do sv start ------
 def dosv():
     kvs=getKvs()
-    # print kvs
+    overwritten=kvs["__overwritten__"]
+    # print kvs["__overwritten__"]
     svs=getSvs(kvs)
     # print svs
-    bovos=getBovos()
-    # print bovos
     for svi in svs:
+        if "yes"!=svi['isEffect']: continue
         ftpn=getCurPath()+svi['template']+".template"
         if not isFile(ftpn):continue
         ft=open(ftpn,"r")
         content=ft.read()
-        for key in kvs.keys(): content=content.replace(key,kvs[key])
-        content=content.replace('__SetBovos__',bovos[0])
-        content=content.replace('__SetVobos__',bovos[1])
-        content=content.replace('__SetBoatoms__',bovos[2])
-        content=content.replace('__SetVosvs__',bovos[3])
-        fopn=getCurPath()+svi['filepath']+svi['filename']
+        for key in kvs.keys():
+            content=content.replace(key,kvs[key])
+        contentExpand=['__AddPropertyDefine__','__AddGetterSetter__','__AddCriteria__']
+        for ce in contentExpand:
+            content=content.replace(ce,getBovos(ce))
+
+        fopn=getCurPath()+svi['filepath']
+        # check out path, if not exists, create it
+        checkOutPath(fopn)
+        fopn=fopn+svi['filename']
+        if overwritten!="true" and not checkOutFile(fopn):fopn=fopn+".temp"
         fo=open(fopn,'w')
         fo.writelines(content)
         fo.flush()
@@ -37,25 +42,26 @@ def getKvs():
     lines=f.readlines()
     kvs={}
     for line in lines:
-        if line.find('=')==-1:continue
+        if line.startswith("#") or line.find('=')==-1:continue
         kv=line.strip().replace('\n','').split('=')
         kvs[kv[0]]=kv[1]
     kvs["@date"]="@date "+getNow()
+    kvs["@Date:"]="@Date: "+getNow()
     f.close()
-    nkvs = {}
+    nkvs={}
     for kv in kvs:
-        nkvs[kv] = kvs[kv]
+        nkvs[kv]=kvs[kv]
         if kv[:2] != '__' and kv[-2:] != '__': continue
-        key = kv.replace('__', '')
-        value = kvs[kv]
+        key=kv.replace('__', '')
+        value=kvs[kv]
         if key[0] >= 'A' and key[0] <= 'Z':
-            nkey = '__' + key[0].lower() + key[1:] + '__'
-            nvalue = value[0].lower() + value[1:]
-            nkvs[nkey] = nvalue
+            nkey='__'+key[0].lower()+key[1:]+'__'
+            nvalue=value[0].lower()+value[1:]
+            nkvs[nkey]=nvalue
         if key != key.lower():
-            nkey = '__' + key.lower() + '__'
-            nvalue = value.lower()
-            nkvs[nkey] = nvalue
+            nkey='__'+key.lower()+'__'
+            nvalue=value.lower()
+            nkvs[nkey]=nvalue
     return nkvs
 
 def getSvs(kvs):
@@ -67,51 +73,82 @@ def getSvs(kvs):
     return svs
 
 
-def getBovos():
-    bovos={}
-    setBovos=''
-    setVobos=''
-    setBoatoms=''
-    setVosvs=''
-    setVosvs2=''
-    i=0
+def getBovos(flag):
+    res=""
     fbo=open(getCurPath()+'/bo.txt','r')
     bolines=fbo.readlines()
-    for boProperty in bolines:
-        boProperty = boProperty.strip().replace('\n', '')
-        if boProperty.find(";") == -1: continue
-        boProperty = boProperty[boProperty.rfind(' ', 0, boProperty.find(';')) + 1:boProperty.find(';')]
-        BoProperty = boProperty[0].upper() + boProperty[1:]
+    for boline in bolines:
+        boline=boline.strip().replace('\n', '')
+        if boline.find(";")==-1: continue
+        boline=boline.strip().replace('	', '')
+        boline=boline.strip().replace(';', '')
+        words=boline.split(" ")
+        if words.__len__()!=3:continue
+        type=words[1]
+        property=words[2]
+        if flag=='__AddPropertyDefine__':
+            res+=getPropertyDefine(type,property)
+        elif flag=='__AddGetterSetter__':
+            res+=getGetterSetter(type,property)
+        elif flag=='__AddCriteria__':
+            res+=getCriteria(type,property)
+        else:
+            continue
+    return res
 
-        i+=1
-        if i!=1:
-            setBovos+='\n\t\t'
-            setVobos+='\n\t\t'
-            setBoatoms+='\n\t\t\t'
-        setBovos+='res.set'+BoProperty+'(VoConvertUtil.getValue(src.get'+BoProperty+'()));'
-        setVobos+='res.set'+BoProperty+'(src.get'+BoProperty+'());'
-        setBoatoms+='if(StringUtils.isNotBlank(request.get'+BoProperty+'())) {criteria.and'+BoProperty+'EqualTo(request.get'+BoProperty+'());}'
-        setVosvs+='\tprivate String '+boProperty+';\n'
-        setVosvs2+='\tpublic String get'+BoProperty+'() {\n\t\treturn '+boProperty+';\n\t}\n\tpublic void set'+BoProperty+'(String '+boProperty+') {\n\t\tthis.'+boProperty+'='+boProperty+';\n\t}\n'
-    bovos[0]=setBovos
-    bovos[1]=setVobos
-    bovos[2]=setBoatoms
-    bovos[3]=setVosvs+'\n\n'+setVosvs2
-    return bovos
+
+def getPropertyDefine(type,property):
+    res="\tprivate "+type+" "+property+";\r\n"
+    return res
+
+def getGetterSetter(type,property):
+    propertyu=property[0].upper()+property[1:len(property)]
+    res=""
+    res+='''\tpublic '''+type+''' get'''+propertyu+'''(){return '''+property+''';}\r\n'''
+    res+='''\tpublic void set'''+propertyu+'''('''+type+''' '''+property+''') {this.'''+property+'''='''+property+''';}\r\n'''
+    return res
+
+def getCriteria(type,property):
+    propertyu=property[0].upper()+property[1:len(property)]
+    res="\t\t\t"
+    if type=="String":
+        res+='''if(!StringUtil.isBlank(request.get'''+propertyu+'''())){criteria.and'''+propertyu+'''EqualTo(request.get'''+propertyu+'''());}'''
+    elif type=="Short" or type=="Integer" or type=="Long" or type=="Float" or type=="Double":
+        res+='''if(request.get'''+propertyu+'''()!=null&&request.get'''+propertyu+'''()>0){criteria.and'''+propertyu+'''EqualTo(request.get'''+propertyu+'''());}'''
+    else :
+        res+='''if(request.get'''+propertyu+'''()!=null){criteria.and'''+propertyu+'''EqualTo(request.get'''+propertyu+'''());}'''
+    res+='\r\n'
+    return res
 
 # ------ do sv end ------
 
 
 
 # ------ base start ------
+
 def getCurPath():
     return os.getcwd()
 
 def isFile(pathname):
     return os.path.isfile(pathname)
 
+def checkOutPath(fop):
+    if not os.path.exists(fop) or not os.path.isdir(fop):
+        os.makedirs(fop)
+
+def checkOutFile(fopn):
+    # not exist, can be created
+    if not os.path.exists(fopn):
+        return True
+    # not a file, can be created
+    elif not os.path.exists(fopn):
+        return True
+    else:
+        return False
+
 def getNow():
     return time.strftime("%Y-%m-%d %H:%M",time.localtime())
+
 # ------ base end ------
 
 
